3850204771546652a4e59743263aa295c348403d
1-21-01-E043-10\POO <paul.cabrera@epn.edu.ec>
2024-06-20 09:54:06 -0500

    Main cambio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String primer_numero= JOptionPane.showInputDialog("Introduzca el primer entero");
+        String segundo_numero=JOptionPane.showInputDialog("Introduzca el segundo entero");
+
+        int numero1= Integer.parseInt(primer_numero);
+        int numero2= Integer.parseInt(segundo_numero);
+        int resultados= numero1+numero2;
+
+        JOptionPane.showMessageDialog(null,"El resultado de la primer entero es: "+resultados,"Resultado",JOptionPane.INFORMATION_MESSAGE);
+
     }
+
+
+
 
 }
--- src/form1.java
+++ src/form1.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 
 public class form1 {
-    private JButton button1;
+    private JButton HOLA;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
